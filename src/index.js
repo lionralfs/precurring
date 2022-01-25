abf6b7ac1e466b7e338ddb1cfb88ab747d32c92c
Lion Ralfs <lion.ralfs@gmail.com>
2022-01-25 13:00:43 +0100

    use real space

--- src/index.js
+++ src/index.js
@@ -61,7 +61,7 @@ function callWithTimeout(fn, timeout) {
  * @property {number} interval Time in ms representing the time span between the last Promise settling and the next one firing
  * @property {number} [timeout] The maximum amount of time in ms which the promise is allowed to take
  * @property {(value: K) => any} onSuccess The success callback
- * @property {(reason: K | Error) => any} onError The error callback
+ * @property {(reason: K | Error) => any} onError The error callback
  */
 
 /**
